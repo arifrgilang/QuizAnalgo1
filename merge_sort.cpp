a085d405430c23c3ce634390a855af263068fa0c
Arif R Gilang <arifrgilang@gmail.com>
2019-03-20 12:03:50 +0700

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,26 +1,24 @@
-/* C program for Merge Sort */
 #include<stdlib.h> 
-#include<stdio.h> 
-  
-// Merges two subarrays of arr[]. 
-// First subarray is arr[l..m] 
-// Second subarray is arr[m+1..r] 
+#include<stdio.h>
+#include<iostream>
+#include<chrono> 
+
+using namespace std;
+using namespace std::chrono; 
+
 void merge(int arr[], int l, int m, int r) 
 { 
     int i, j, k; 
     int n1 = m - l + 1; 
     int n2 =  r - m; 
   
-    /* create temp arrays */
     int L[n1], R[n2]; 
   
-    /* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++) 
         L[i] = arr[l + i]; 
     for (j = 0; j < n2; j++) 
         R[j] = arr[m + 1+ j]; 
   
-    /* Merge the temp arrays back into arr[l..r]*/
     i = 0; // Initial index of first subarray 
     j = 0; // Initial index of second subarray 
     k = l; // Initial index of merged subarray 
@@ -38,9 +36,7 @@ void merge(int arr[], int l, int m, int r)
         } 
         k++; 
     } 
-  
-    /* Copy the remaining elements of L[], if there 
-       are any */
+
     while (i < n1) 
     { 
         arr[k] = L[i]; 
@@ -48,8 +44,6 @@ void merge(int arr[], int l, int m, int r)
         k++; 
     } 
   
-    /* Copy the remaining elements of R[], if there 
-       are any */
     while (j < n2) 
     { 
         arr[k] = R[j]; 
@@ -58,17 +52,12 @@ void merge(int arr[], int l, int m, int r)
     } 
 } 
   
-/* l is for left index and r is right index of the 
-   sub-array of arr to be sorted */
 void mergeSort(int arr[], int l, int r) 
 { 
     if (l < r) 
     { 
-        // Same as (l+r)/2, but avoids overflow for 
-        // large l and h 
         int m = l+(r-l)/2; 
-  
-        // Sort first and second halves 
+
         mergeSort(arr, l, m); 
         mergeSort(arr, m+1, r); 
   
@@ -76,8 +65,6 @@ void mergeSort(int arr[], int l, int r)
     } 
 } 
   
-/* UTILITY FUNCTIONS */
-/* Function to print an array */
 void printArray(int A[], int size) 
 { 
     int i; 
@@ -85,19 +72,31 @@ void printArray(int A[], int size)
         printf("%d ", A[i]); 
     printf("\n"); 
 } 
-  
-/* Driver program to test above functions */
+
 int main() 
 { 
-    int arr[] = {12, 11, 13, 5, 6, 7}; 
-    int arr_size = sizeof(arr)/sizeof(arr[0]); 
+    int arr_size;
+    cout<<"Input array size: "<<endl;
+    cin>>arr_size;
+    
+    int arr[arr_size];
+    cout<<"Input data:"<<endl;
+    
+    for(int i = 0; i<arr_size; i++)
+        cin>>arr[i];
+    
+    // int arr[] = {12, 11, 13, 5, 6, 7}; 
+    // int arr_size = sizeof(arr)/sizeof(arr[0]); 
   
-    printf("Given array is \n"); 
+    printf("Array yang di input adalah: \n"); 
     printArray(arr, arr_size); 
-  
+    auto start = high_resolution_clock::now(); 
     mergeSort(arr, 0, arr_size - 1); 
-  
-    printf("\nSorted array is \n"); 
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop-start); 
+    
+    printf("\nArray yang telah di sort adalah: \n"); 
     printArray(arr, arr_size); 
+    cout << "Runtime merge sort function : " << duration.count() << " microseconds" << endl;
     return 0; 
 } 
